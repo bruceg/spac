7f05cd827e859dfb80d7e831da4a7e2e753eb4c4
Bruce Guenter <bruce@untroubled.org>
2007-07-07 05:30:32 +0000

    Prefer python2 over python when making =py executables.

--- rules/python-head.py
+++ rules/python-head.py
@@ -1,5 +1,5 @@
 :
 ( set -e; PATH="/bin:/usr/bin:/usr/local/bin:$$PATH"; export PATH; \
-  python=`which python`; \
+  python=`which python2 || which python 2>/dev/null`; \
   echo "#! $$python"; \
   echo "# WARNING: This file was auto-generated. Do not edit!" ) >python-head.py
