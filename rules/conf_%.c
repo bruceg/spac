5b0e0aecafa92af40629220f3238b92f055d5493
Bruce Guenter <bruce@untroubled.org>
2008-04-09 14:33:02 -0600

    Handle conf-* files with dashes in them in rules/conf_%.c

--- rules/conf_%.c
+++ rules/conf_%.c
@@ -1,5 +1,7 @@
-: conf-%(stem)s
-head -n 1 conf-%(stem)s | \
+conffile = 'conf-' + stem.replace('_', '-')
+dependon([conffile])
+:
+head -n 1 %(conffile)s | \
   sed -e 's/"/\\"/g' \
       -e 's/^/const char conf_%(stem)s[] = "/' \
       -e 's/$$/";/' >conf_%(stem)s.c
